c8c85478c9054ba79f924bd5d213cc770a12d7ef
Facundo Serna <facuserna93@gmail.com>
2025-08-31 17:34:54 -0300

    Deleted useless files

--- scripts/LevelSelect.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var level1_button: Button = $VBox/Level1Button
-
-func _ready():
-	level1_button.pressed.connect(_on_level1)
-
-func _on_level1():
-	get_tree().change_scene_to_file("res://scenes/levels/Level1_Modular.tscn")
